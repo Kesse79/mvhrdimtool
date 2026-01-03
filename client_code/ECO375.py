82d459b2c24ffa95403b0e639f757358fed26f64
cla@metrotherm.dk <cla@metrotherm.dk>
2026-01-03 19:11:31 +0000

    Created eco175.png; Edited ECO175PET, ECO190XL, ECO360, ECO375 and 1 more

--- client_code/ECO375.py
+++ client_code/ECO375.py
@@ -40,13 +40,3 @@ class ECO375(ECO375Template):
     if sender and sender.tag:
       open_form(sender.tag)
 
-  @handle("flow_input", "pressed_enter")
-  def flow_input_pressed_enter(self, **event_args):
-
-    """This method is called when the user presses Enter in this text box"""
-    pass
-
-  @handle("flow_input", "focus")
-  def flow_input_focus(self, **event_args):
-
-    pass
