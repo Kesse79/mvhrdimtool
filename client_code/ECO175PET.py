125272cecca58c6bc891f9d7a436bd5256923fd3
Kesse79 <131038282+Kesse79@users.noreply.github.com>
2026-01-03 17:18:32 +0100

    Fix sidebar navigation between MVHR units

--- client_code/ECO175PET.py
+++ client_code/ECO175PET.py
@@ -34,3 +34,8 @@ class ECO175PET(ECO175PETTemplate):
       if hasattr(self, 'out_sfp'): self.out_sfp.content = msg
       if hasattr(self, 'out_sound'): self.out_sound.content = msg
       if hasattr(self, 'out_heat'): self.out_heat.content = msg
+
+  def nav_link_click(self, **event_args):
+    sender = event_args.get('sender')
+    if sender and sender.tag:
+      open_form(sender.tag)
